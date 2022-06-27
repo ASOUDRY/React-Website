b4831c43d461e046db30ce27ec86635cd3ebe22f
Alexander Soudry <soudryalex@gmail.com>
2022-06-27 12:59:54 -0400

    Updates

---
+++ precache-manifest.437e451e44047cc9d8b2355808f89cab.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e416618af7a0a4e3f6467b9dc1f77e23",
+    "url": "/React-Website/index.html"
+  },
+  {
+    "revision": "d4afbc61eb617885bb90",
+    "url": "/React-Website/static/css/2.f1ea7a3d.chunk.css"
+  },
+  {
+    "revision": "41d3a2982229764ba2b5",
+    "url": "/React-Website/static/css/main.a3044a1d.chunk.css"
+  },
+  {
+    "revision": "d4afbc61eb617885bb90",
+    "url": "/React-Website/static/js/2.bc0c7683.chunk.js"
+  },
+  {
+    "revision": "41d3a2982229764ba2b5",
+    "url": "/React-Website/static/js/main.87e6c6a1.chunk.js"
+  },
+  {
+    "revision": "dd39693b7650e669a9bc",
+    "url": "/React-Website/static/js/runtime~main.c3988113.js"
+  },
+  {
+    "revision": "e7c116cf4c972ee1f15c02310ab71bd5",
+    "url": "/React-Website/static/media/dog.e7c116cf.png"
+  },
+  {
+    "revision": "75bc0224d1c1f0d19d4bac951e6f89e7",
+    "url": "/React-Website/static/media/forum.75bc0224.png"
+  },
+  {
+    "revision": "827de1ab4e786675e239b2947280602f",
+    "url": "/React-Website/static/media/movie.827de1ab.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/React-Website/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/React-Website/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/React-Website/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/React-Website/static/media/slick.f97e3bbf.svg"
+  }
+]);
